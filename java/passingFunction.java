8fffbf1e1a65995878a81beb353394b56d92a411
Dhirajkumar1811 <92907198+Dhirajkumar1811@users.noreply.github.com>
2022-03-25 22:48:42 +0530

    Add files via upload

---
+++ java/passingFunction.java
@@ -0,0 +1,16 @@
+package com.java;
+
+import java.util.Arrays;
+
+public class passingFunction {
+    public static void main(String[] args) {
+        int[] nums = {4,5,6,8,7};
+        System.out.println(Arrays.toString(nums));
+        change(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+    static void change(int[] arr){
+        arr[0] = 1828;
+    }
+
+}
