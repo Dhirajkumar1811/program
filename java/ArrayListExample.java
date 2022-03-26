1fb4bbc12378573fb747aedf6f58d5ab935273d1
Dhirajkumar1811 <92907198+Dhirajkumar1811@users.noreply.github.com>
2022-03-27 00:53:40 +0530

    Add files via upload

---
+++ java/ArrayListExample.java
@@ -0,0 +1,33 @@
+package com.java;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ArrayListExample {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+       ArrayList<Integer> list = new ArrayList<>(10);
+//        list.add(10);
+//        list.add(10);
+//        list.add(10);
+//        list.add(10);
+//        list.add(11);
+//        list.add(10);
+//        list.add(10);
+//        list.add(10);
+//        list.add(10);
+
+//        System.out.println(list.contains(11));
+//        list.set(0,18);
+//        System.out.println(list);
+
+        for (int i = 0; i < 5; i++) {
+            list.add(in.nextInt());
+        }
+        // get item any index
+        for (int i = 0; i < 5; i++) {
+            System.out.println(list.get(i));
+        }
+        System.out.println(list);
+    }
+}
