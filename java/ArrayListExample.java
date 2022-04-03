e816afd3edbb2078797e63d1a3e0189263f11ed5
Dhirajkumar1811 <92907198+Dhirajkumar1811@users.noreply.github.com>
2022-04-04 00:01:35 +0530

    Update ArrayListExample.java

--- java/ArrayListExample.java
+++ java/ArrayListExample.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ArrayListExample {
-    public    static void main(String[] args) {
+    public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
        ArrayList<Integer> list = new ArrayList<>(10);
 //        list.add(10);
