24c201909f18f2e490498a6ec3913253621ce0ba
Dhirajkumar1811 <92907198+Dhirajkumar1811@users.noreply.github.com>
2022-04-12 23:48:39 +0530

    Add files via upload

---
+++ c class/pyr.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+void pyramid(int n)
+{
+    int i,j;
+    for(i=n;i>=1;i--)
+    {
+        printf("\n");
+        for(j=1;j<=i;j++)
+        {
+            printf(" %d ",j);
+        }
+        }
+}
+int read()
+{
+   int n;
+   printf("\n\t Enter the No.: ");
+   scanf("%d",&n);
+   return n;
+}
+int main()
+{
+    int n;
+    n=read();
+    pyramid(n);
+}
