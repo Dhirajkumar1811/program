fdf96d9ffc32a3a3b2fe9d4bb515ebfb3a25849f
Dhirajkumar Sonawane <92907198+Dhirajkumar1811@users.noreply.github.com>
2022-04-26 23:22:11 +0530

    Add files via upload

---
+++ cpp code/exception.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    double num,den,divi;
+    cout<<"\n Division";
+    cout<<"\n Enter nemarator & denometer : ";
+    cin>>num>>den;
+    try{
+        if(den==0){
+            throw den;
+        }else {
+            divi = num/den;
+        }
+    }
+    catch(double ex){
+        cout<<"\n Exception : denomentor shouldn't be Zero ";
+    }
+    cout<<"\n Division is : "<<divi;
+    return 0;
+}
